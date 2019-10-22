0a31af46ffb4aeaea80a8b3843ac6903b8626bf8
Will <Will.irvin18@gmail.com>
2019-10-22 17:28:50 -0400

    finished up making it look pretty, made actual questions

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,16 +3,16 @@ var friendsArray = [
         "name" : "Mr. Krabs",
         "photo" : "https://www.meme-arsenal.com/memes/7894779fdeb9b421942553c85da1fc43.jpg",
         "scores" : [
-            5,
-            3,
+            1,
+            1,
+            1,
             5,
             1,
-            2,
-            4,
-            2,
-            4,
-            3,
-            5
+            1,
+            5,
+            1,
+            1,
+            1
         ]
     },
 
@@ -20,16 +20,16 @@ var friendsArray = [
         "name" : "Hey Arnold",
         "photo" : "https://upload.wikimedia.org/wikipedia/en/thumb/2/2a/HeyArnold%21.jpg/150px-HeyArnold%21.jpg",
         "scores" : [
-            3, 
-            2,
-            4,
+            1, 
             1,
             5,
-            2,
-            5,
-            2,
             1,
-            5
+            1,
+            1,
+            1,
+            1,
+            5,
+            1
         ]
     },
 
@@ -37,16 +37,16 @@ var friendsArray = [
         "name" : "Cosmo",
         "photo" : "https://vignette.wikia.nocookie.net/fairlyoddfanon/images/1/1a/Cosmo_Stock_Image.png/revision/latest?cb=20180509234547",
         "scores" : [
-            4,
-            2,
-            2,
             1,
             5,
-            4,
             1,
-            3,
-            4,
-            4
+            1,
+            1,
+            5,
+            1,
+            5,
+            1,
+            1
         ]
     },
 
@@ -54,15 +54,15 @@ var friendsArray = [
         "name" : "That Guy From Rocket Power",
         "photo" : "https://vignette.wikia.nocookie.net/homiestuck/images/b/b3/Tito.png/revision/latest?cb=20190420042453",
         "scores" : [
-            3,
-            1,
-            4,
             5,
-            2,
             1,
-            3,
+            1,
+            1,
             5,
-            2,
+            1,
+            1,
+            1,
+            1,
             1
         ]
     },
@@ -71,16 +71,16 @@ var friendsArray = [
         "name" : "Wanda",
         "photo" : "https://vignette.wikia.nocookie.net/fairlyoddfanon/images/7/7e/Wanda_flash_animation.png/revision/latest?cb=20180826042321",
         "scores" : [
-            4,
-            2,
-            5,
-            2,
             1,
-            3,
-            4,
-            2,
-            3,
-            2
+            5,
+            1,
+            1,
+            1,
+            5,
+            1,
+            1,
+            1,
+            5
         ]
     },
     
